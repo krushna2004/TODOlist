5f58466d20bccfa28bc660eb49479b63fa9d1aaf
krushna2004 <krushnakanani23@gmail.com>
2023-09-15 17:39:15 +0530

    [Add] todo list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [curTask, setCurTask] = useState('');
+  const [allTask, setAllTask] = useState([]);
+  const [count, setcount] = useState(0);
+  const btnClick =()=>{
+    let templist=[...allTask,curTask];
+    setAllTask(templist);
+    setCurTask('');
+    setcount(count+1);
+  }
+  function dlt(index){
+    let templist=[...allTask];
+    templist.splice(index,1);
+    setcount(count-1);
+    setAllTask(templist);
 
+  }
+
+
+  return (
+    
+<>
+    <h1>Todo App</h1>
+    <div>
+      Enter Task:
+      <br/>
+      <input type='text' placeholder='Enter here' value={curTask} onChange={(e)=>setCurTask(e.target.value)}/>
+      <button onClick={btnClick}>Add</button>
+      <br/>
+    </div>
+    <div>
+      <h4>Total Tasks: {count}</h4>
+      <h3>Tasks:</h3>
+      <ol>
+        {allTask.map((task,i)=><li>{task}<button onClick={dlt}>Delete</button></li>)}
+      </ol>
+    </div>
+    </>
+
+
+  );
+
+  }
 export default App;
